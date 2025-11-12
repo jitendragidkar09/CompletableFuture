11fb174c50b020336da83b08ee9db3b8db4c9b08
Flatmate-Fight_Resolver <jitendragr09@gmail.com>
2025-11-12 17:27:28 +0530

    Initial Commit

---
+++ src/main/java/org/example/database/EmployeeDatabase.java
@@ -0,0 +1,23 @@
+package org.example.database;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.dto.Employee;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class EmployeeDatabase {
+    public static List<Employee> fetchEmployees() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper
+                    .readValue(new File("employees.json"), new TypeReference<List<Employee>>() {
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
